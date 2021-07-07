e997f72c16819aebf3d558ef1d4971c92565af75
Kyle Winkler <kyle@caxy.com>
2021-07-07 17:18:08 -0500

    added auth protection

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { connect } from './utils/db'
 import userRouter from './resources/user/user.router'
 import itemRouter from './resources/item/item.router'
 import listRouter from './resources/list/list.router'
+import { protect } from './utils/auth'
 
 export const app = express()
 
@@ -17,6 +18,7 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+app.use('/api', protect)
 app.use('/api/user', userRouter)
 app.use('/api/item', itemRouter)
 app.use('/api/list', listRouter)
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,67 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'email and password required' })
+  }
 
-export const signin = async (req, res) => {}
+  try {
+    const user = await User.create(req.body)
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (e) {
+    console.error(e)
+    return res.status(400).end()
+  }
+}
+
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'email and password required' })
+  }
+
+  const user = await User.findOne({ email: req.body.email }).exec()
+
+  if (!user) {
+    return res.status(401).send({ message: 'bad login info' })
+  }
+
+  try {
+    const match = await user.checkPassword(req.body.password)
+
+    if (!match) {
+      return res.status(401).send({ message: 'bad login info' })
+    }
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (e) {
+    console.error(e)
+    return res.status(401).send({ message: 'bad login info' })
+  }
+}
 
 export const protect = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).end()
+  }
+
+  let token = req.headers.authorization.split('Bearer ')[1]
+  if (!token) {
+    return res.status(401).end()
+  }
+
+  try {
+    const payload = await verifyToken(token)
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean()
+      .exec()
+    req.user = user
+    next()
+  } catch (e) {
+    console.error(e)
+    return res.status(401).end()
+  }
   next()
 }
